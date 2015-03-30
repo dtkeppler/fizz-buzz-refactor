2b723265c1c48d247925ee6728e4ef06d40407cd
Dustin Keppler <dtkeppler@yahoo.com>
2015-03-30 18:51:40 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function fizzbuzz(max) {
+  for ( var i = 1; i <= max; i++ ) {
+    if (i % 5 == 0 && i % 3 == 0) {
+      document.write("FizzBuzz");
+    } else if ( i % 3 == 0 ) {
+      document.write("Fizz");
+    } else if (i % 5 == 0) {
+      document.write("Buzz");
+    } else {
+      document.write(i);
+    }
+  }
+}
+
+
+var userInput = prompt("Please enter a number");
+var max = parseInt(userInput);
+
