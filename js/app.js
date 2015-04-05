e42c32e24e9f7ebe005c7a6a2b83db3c620f3c3a
Dustin Keppler <dtkeppler@yahoo.com>
2015-04-05 10:55:34 -0400

    refactored for user-defined max number

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
+var userInput = prompt("Please enter a number");
+var max = parseInt(userInput);
+
+fizzbuzz(max)
+
 function fizzbuzz(max) {
   for ( var i = 1; i <= max; i++ ) {
     if (i % 5 == 0 && i % 3 == 0) {
-      document.write("FizzBuzz");
+      document.write("FizzBuzz" + ", ");
     } else if ( i % 3 == 0 ) {
-      document.write("Fizz");
+      document.write("Fizz" + ", ");
     } else if (i % 5 == 0) {
-      document.write("Buzz");
+      document.write("Buzz" + ", ");
     } else {
-      document.write(i);
+      document.write(i + ", ");
     }
   }
 }
 
-
-var userInput = prompt("Please enter a number");
-var max = parseInt(userInput);
 
